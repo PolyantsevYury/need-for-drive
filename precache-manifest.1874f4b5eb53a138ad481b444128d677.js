d9eb4d988ec128e85922a0ec7af08f45e339cc2a
yury_mac <polyantsevyury@gmail.com>
2020-07-08 23:06:23 +0400

    Updates

---
+++ precache-manifest.1874f4b5eb53a138ad481b444128d677.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f4bb6c6ab5ed3f398a8c63b772e06c7",
+    "url": "/need-for-drive/index.html"
+  },
+  {
+    "revision": "12a5ac7ca8ac2d8eeff8",
+    "url": "/need-for-drive/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d0a7d60b7d9a8357a5cb",
+    "url": "/need-for-drive/static/js/2.3b6d169e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/need-for-drive/static/js/2.3b6d169e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12a5ac7ca8ac2d8eeff8",
+    "url": "/need-for-drive/static/js/main.d394a221.chunk.js"
+  },
+  {
+    "revision": "9725ee61d70b674aa54c",
+    "url": "/need-for-drive/static/js/runtime-main.c0011e84.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/need-for-drive/static/media/logo.5d5d9eef.svg"
+  }
+]);
