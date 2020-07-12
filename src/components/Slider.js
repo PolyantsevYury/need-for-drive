1b976b234d58cecc2185a1ed908e3c9cb9289d5e
Yuri-codes <52564127+Yuri-codes@users.noreply.github.com>
2020-07-12 19:15:58 +0400

    add slider layout

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -3,7 +3,25 @@ import '../styles/mainPage/Slider.scss'
 
 export const Slider = () => {
   return (
-      <div className='slider'>
-      </div>
+      <section className='slider'>
+        <button className='slider__control slider__control-prev'>
+        </button>
+        <div className='slider__content'>
+          <div className='slider__item'>
+            <h2 className='slider__item-title'>Бесплатная парковка</h2>
+            <p className='slider__item-info'>Оставляйте машину на платных городских
+              парковках и разрешенных местах, не нарушая ПДД, а также в аэропортах.</p>
+            <button className='slider__item-button'>Подробнее</button>
+          </div>
+          <div className='slider__nav'>
+            <button className='slider__nav-item'> </button>
+            <button className='slider__nav-item slider__nav-item--active'> </button>
+            <button className='slider__nav-item'> </button>
+            <button className='slider__nav-item'> </button>
+          </div>
+        </div>
+        <button className='slider__control slider__control-next'>
+        </button>
+      </section>
   )
 };
