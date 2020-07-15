cd84074ac83a16b04a1ef1deabb1af6e15c46a29
Yuri-codes <52564127+Yuri-codes@users.noreply.github.com>
2020-07-15 17:26:37 +0400

    add adaptive layout for desktop, laptop, and tablet

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import './App.scss';
+import { useMediaQuery } from 'react-responsive'
 
 import {SideBar} from "./components/mainPage/sidebar/Sidebar";
 import {MainContent} from "./components/mainPage/mainContent/MainContent";
 import {Slider} from "./components/mainPage/slider/Slider";
 
 const App = () => {
+  const isLaptopOrDesktop = useMediaQuery({ minWidth: 1024 })
   return (
       <div className='app-wrapper'>
           <SideBar/>
           <MainContent/>
-          <Slider/>
+        {isLaptopOrDesktop && <Slider/>}
       </div>
   )
 };
