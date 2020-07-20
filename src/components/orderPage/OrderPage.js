882abc7704a75d7b118ef203fcda4699e5affa49
yury_mac <polyantsevyury@gmail.com>
2020-07-21 01:23:55 +0400

    Add Steps component, add adaptive for order page

--- src/components/orderPage/OrderPage.js
+++ src/components/orderPage/OrderPage.js
@@ -1,11 +1,15 @@
 import React from "react";
 import {Header} from "../common/header/Header";
 import './OrderPage.scss'
+import {Steps} from "./Steps";
 
 export const OrderPage = () => {
   return (
       <section className='order-page'>
-        <Header/>
+        <div className='order-page__header'>
+          <Header/>
+        </div>
+        <Steps/>
       </section>
   )
 };
