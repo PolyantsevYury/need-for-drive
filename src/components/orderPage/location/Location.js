0b47f290c99f8290f39e1bdfe01c3fde0af010ff
yury_mac <polyantsevyury@gmail.com>
2020-07-22 10:26:57 +0400

    add adaptive and responsive for all screens, fix bugs

--- src/components/orderPage/location/Location.js
+++ src/components/orderPage/location/Location.js
@@ -1,6 +1,7 @@
 import React from "react";
 import './Location.scss'
 import Map from '../../../assets/images/map.jpg';
+import Clean from '../../../assets/images/icons/clean_icon.svg';
 
 export const Location = () => {
   return (
@@ -8,9 +9,11 @@ export const Location = () => {
         <div className='location__input-items'>
           <label className='location__input-item'>Город
             <input placeholder='Начните вводить город' name="city" type="text"/>
+            <img src={Clean} alt=""/>
           </label>
           <label className='location__input-item'>Пункт выдачи
-          <input placeholder='Начните вводить пункт' name='address' type="text"/>
+            <input placeholder='Начните вводить пункт' name='address' type="text"/>
+            <img src={Clean} alt=""/>
           </label>
         </div>
         <p>Выбрать на карте:</p>
