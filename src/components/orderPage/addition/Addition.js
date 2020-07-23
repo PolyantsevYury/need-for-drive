827862610a861352e6223472a8b3f3d115b8147b
yury_mac <polyantsevyury@gmail.com>
2020-07-23 20:38:21 +0400

    add forms for radio, checkbox and text inputs, refactoring styles

--- src/components/orderPage/addition/Addition.js
+++ src/components/orderPage/addition/Addition.js
@@ -1,72 +1,48 @@
 import React from "react";
 import './Addition.scss'
-import Clean from "../../../assets/images/icons/clean_icon.svg";
+import {InputCheckbox, InputRadio, InputText} from "../../common/forms/Forms";
 
 export const Addition = () => {
   return (
       <section className='addition'>
-        <div className='addition__item'>
+        <div className='addition__option'>
           <h4 className='addition__title'>Цвет</h4>
-          <div className='addition__filter'>
-            <div className='addition__filter-item'>
-              <input type="radio" id='any' defaultChecked={true} name='color' value='any'/>
-              <label htmlFor='any'>Любой</label>
-            </div>
-            <div className='addition__filter-item'>
-              <input type="radio" id='red' name='color' value='red'/>
-              <label htmlFor='red'>Красный</label>
-            </div>
-            <div className='addition__filter-item'>
-              <input type="radio" id='blue' name='color' value='blue'/>
-              <label htmlFor='blue'>Голубой</label>
-            </div>
-          </div>
+          <InputRadio name='color'
+                      items={[
+                        {label: 'Любой', value: 'any', defaultChecked: true},
+                        {label: 'Красный', value: 'red'},
+                        {label: 'Голубой', value: 'blue'},
+                      ]}/>
         </div>
 
-        <div className='addition__item'>
+        <div className='addition__option'>
           <h4 className='addition__title'>Дата аренды</h4>
-          <div className='addition__input-items'>
-            <label className='addition__input-item'>C
-              <input placeholder='Введите дату и время' name="from" type="text"/>
-              <img src={Clean} alt=""/>
-            </label>
-            <label className='addition__input-item'>По
-              <input placeholder='Введите дату и время' name='to' type="text"/>
-              <img src={Clean} alt=""/>
-            </label>
-          </div>
+          <InputText name='date'
+                     items={[
+                       {label: 'С', placeholder: 'Введите дату и время'},
+                       {label: 'По', placeholder: 'Введите дату и время'},
+                     ]}/>
         </div>
 
-        <div className='addition__item'>
+        <div className='addition__option'>
           <h4 className='addition__title'>Тариф</h4>
-          <div className='addition__filter addition__filter-tarif'>
-            <div className='addition__filter-item'>
-              <input type="radio" id='minute' name='tarif' value='minute'/>
-              <label htmlFor='minute'>Поминутно, 7₽/мин</label>
-            </div>
-            <div className='addition__filter-item'>
-              <input type="radio" id='day' name='tarif' value='day'/>
-              <label htmlFor='day'>На сутки, 1999 ₽/сутки</label>
-            </div>
-          </div>
+          <InputRadio name='plan'
+                      direction='column'
+                      items={[
+                        {label: 'Поминутно, 7₽/мин', value: 'minute'},
+                        {label: 'На сутки, 1999 ₽/сутки', value: 'day', defaultChecked: true},
+                      ]}/>
         </div>
 
-        <div className='addition__item'>
+        <div className='addition__option'>
           <h4 className='addition__title'>Доп услуги</h4>
-          <div className='addition__filter addition__filter-tarif'>
-            <div className='addition__filter-item'>
-              <input type="checkbox" id='fuel' value='fuel'/>
-              <label htmlFor='fuel'>Полный бак, 500р</label>
-            </div>
-            <div className='addition__filter-item'>
-              <input type="checkbox" id='chair' value='chair'/>
-              <label htmlFor='chair'>Детское кресло, 200р</label>
-            </div>
-            <div className='addition__filter-item'>
-              <input type="checkbox" id='wheel' value='wheel'/>
-              <label htmlFor='wheel'>Правый руль, 1600р</label>
-            </div>
-          </div>
+          <InputCheckbox name='extra-options'
+                         direction='column'
+                         items={[
+                           {label: 'Полный бак, 500р', value: 'fuel'},
+                           {label: 'Детское кресло, 200р', value: 'chair'},
+                           {label: 'Правый руль, 1600р', value: 'wheel'},
+                         ]}/>
         </div>
       </section>
   )
