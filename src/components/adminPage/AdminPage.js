daa1d0bd919d2357dad5568d0c2e9545cd3b1ca5
yury_mac <polyantsevyury@gmail.com>
2020-08-24 23:29:09 +0400

    Add header

--- src/components/adminPage/AdminPage.js
+++ src/components/adminPage/AdminPage.js
@@ -18,7 +18,14 @@ const AdminPage = ({ location }) => {
         <div className="admin__content content">
           {location.pathname === "/admin/orders" && <Orders />}
         </div>
-        <div className="admin__footer"> </div>
+        <div className="admin__footer">
+          <a className="admin__footer-link" href="/">
+            Главная страница
+          </a>
+          <span className="admin__footer-copyright">
+            Copyright © 2020 Simbirsoft
+          </span>
+        </div>
       </div>
     </div>
   );
