74c1ef3a7d954d92c101e5b79402b563061a4266
yury_mac <polyantsevyury@gmail.com>
2020-08-25 23:43:21 +0400

    Add adaptive/responsive content and footer

--- src/components/adminPage/orders/Orders.js
+++ src/components/adminPage/orders/Orders.js
@@ -2,7 +2,13 @@ import React from "react";
 import "./Orders.scss";
 import { Form, Formik } from "formik";
 import { Button } from "../../common/buttons/Buttons";
-import { Filter } from "../../common/forms/Forms";
+import { Checkbox, Filter } from "../../common/forms/Forms";
+import modelExample from "../../../assets/images/adminModel.png";
+import approveIcon from "../../../assets/images/icons/approve_icon.svg";
+import rejectIcon from "../../../assets/images/icons/reject_icon.svg";
+import editIcon from "../../../assets/images/icons/edit_icon.svg";
+import nextPage from "../../../assets/images/icons/next_page_icon.svg";
+import prevPage from "../../../assets/images/icons/prev_page_icon.svg";
 
 const Orders = () => {
   const periodOptions = [
@@ -35,6 +41,8 @@ const Orders = () => {
   };
   // eslint-disable-next-line no-console
   const onFilterSubmit = (value) => console.log(value);
+  // eslint-disable-next-line no-console
+  const onCheck = () => console.log("Check");
 
   return (
     <div className="orders">
@@ -59,8 +67,71 @@ const Orders = () => {
             </Form>
           </Formik>
         </div>
-        <div className="orders__content">f</div>
-        <div className="orders__footer">f</div>
+        <div className="orders__content">
+          <div className="order">
+            <div className="container">
+              <img className="order__img" src={modelExample} alt="" />
+              <div className="order__info">
+                <p className="order__info-item">
+                  <span>ELANTRA</span> в <span>Ульяновск</span>, Нариманова 42
+                </p>
+                <p className="order__info-item">
+                  12.06.2019 12:00 — 13.06.2019 12:00
+                </p>
+                <p className="order__info-item">
+                  Цвет: <span>Голубой</span>
+                </p>
+              </div>
+            </div>
+            <div className="container">
+              <Checkbox
+                direction="column"
+                items={[
+                  {
+                    label: "Полный бак",
+                    value: "fullFuel",
+                    checked: true,
+                  },
+                  {
+                    label: "Детское кресло",
+                    value: "childSeat",
+                    checked: false,
+                  },
+                  {
+                    label: "Правый руль",
+                    value: "rightHand",
+                    checked: true,
+                  },
+                ]}
+                onChange={onCheck}
+              />
+              <div className="order__price">4 300 ₽</div>
+            </div>
+            <div className="order__actions">
+              <button className="order__approve-btn" type="button">
+                <img src={approveIcon} alt=" " />
+                Готово
+              </button>
+              <button className="order__reject-btn" type="button">
+                <img src={rejectIcon} alt=" " />
+                Отмена
+              </button>
+              <button className="order__edit-btn" type="button">
+                <img src={editIcon} alt=" " />
+                Изменить
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="orders__footer">
+          <div className="pages">
+            <img src={prevPage} alt="" />
+            <span className="pages__item">1</span>
+            <span className="pages__item pages__item--active">2</span>
+            <span className="pages__item">3</span>
+            <img src={nextPage} alt="" />
+          </div>
+        </div>
       </div>
     </div>
   );
